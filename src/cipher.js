a4d4e797888478f827766052cf3cecb0f8853627
Mayra Romero <nahytar@gmail.com>
2018-05-30 10:56:23 -0400

    codificado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,22 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (text, positions)=> {
+    let charCode;
+    let result = "";
+    for(let i = 0; i < text.length; i++) {
+      charCode = (text.charCodeAt(i) - 65 + positions) % 26 + 65;
+      result += String.fromCharCode(charCode);
+    }
+
+    return result;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (text, positions) => {
+    let charCode;
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      charCode = (text.charCodeAt(i) - 65 - positions) % 26 + 65;
+      result += String.fromCharCode(charCode);
+    }
+
+    return result;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 window.onload = function () {
     // toma del HTML el numero de posiciones que el usuario quiere desplazar
     let inputPosiciones = document.getElementById("inputPosiciones");
+    let buttonReversible = document.getElementById("buttonReversible");
+    let areaDecodificado = document.getElementById("areaDecodificado");
+    let areaCodificado = document.getElementById("areaCodificado");
     // el keyup es el evento que se dispara cuando la tecla sube
     inputPosiciones.addEventListener("keyup" , function (event) {
         // parseInt toma solo los numero enteros que el usuario escribe
@@ -14,4 +17,9 @@ window.onload = function () {
             inputPosiciones.value = positions;
         }
     });
+
+    buttonReversible.addEventListener("click", function (event) {
+        let positions = parseInt(inputPosiciones.value);
+        areaCodificado.value = cipher.encode(areaDecodificado.value, positions); 
+    })
 }

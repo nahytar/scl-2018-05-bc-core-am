56666a51886586beeeac69707c80133c2270e652
Mayra Romero <nahytar@gmail.com>
2018-05-31 23:14:15 -0400

    se discrimina a los caracteres extraños en el codificado y decodificado

--- src/cipher.js
+++ src/cipher.js
@@ -8,10 +8,17 @@ window.cipher = {
     text = text.toUpperCase();
     // el buble for itera por cada letra del string "text"
     for (let i = 0; i < text.length; i++) {
-      // el metodo charCodeAt me pasa la posicion del caracter i en el codigo ascii
-      charCode = (text.charCodeAt(i) - 65 + positions) % 26 + 65;
-      // concatena los caracteres en base charCode calculado
-      result += String.fromCharCode(charCode);
+      // se pregunta si el caracter esta fuera del rango de las letras desde 65 hasta 90
+      if (text.charCodeAt(i) < 65 || text.charCodeAt(i) > 90) {
+        // si esta fuera del rango se concatena el mismo caracter sin procesar
+        result += text[i];
+        // si esta dentro del rango se codifica el caracter
+      } else {
+        // el metodo charCodeAt me pasa la posicion del caracter i en el codigo ascii
+        charCode = (text.charCodeAt(i) - 65 + positions) % 26 + 65;
+        // concatena los caracteres en base charCode calculado
+        result += String.fromCharCode(charCode);
+      }
     }
     // devuelve el string resultante
     return result;
@@ -21,13 +28,16 @@ window.cipher = {
     let result = "";
     text = text.toUpperCase();
     for (let i = 0; i < text.length; i++) {
-
-      charCode = text.charCodeAt(i) - 65 - positions % 26 + 65;
-      // si el charCode quedo antes que el rango alfabeto del codigo ascii se suma 26 para volver a poderlo en rango
-      if (charCode < 65) {
-        charCode += 26;
+      if (text.charCodeAt(i) < 65 || text.charCodeAt(i) > 90) {
+        result += text[i];
+      } else {
+        charCode = text.charCodeAt(i) - 65 - positions % 26 + 65;
+        // si el charCode quedo antes que el rango alfabeto del codigo ascii se suma 26 para volver a poderlo en rango
+        if (charCode < 65) {
+          charCode += 26;
+        }
+        result += String.fromCharCode(charCode);
       }
-      result += String.fromCharCode(charCode);
     }
 
     return result;

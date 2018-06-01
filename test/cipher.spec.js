35924036cb2fb8e61355c3cc1bd9393d851a17cc
Mayra Romero <nahytar@gmail.com>
2018-05-31 23:33:43 -0400

    crean test para controlar minusculas y caracteres especiales

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -14,6 +14,14 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
       assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33),'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz', 33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+
+    it('debería retornar " ?!,.;:-_+=$" para " ?!,.;:-_+=$" con offset 33', () => {
+      assert.equal(cipher.encode(' ?!,.;:-_+=$', 33), ' ?!,.;:-_+=$');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -24,6 +32,14 @@ describe('cipher', () => {
   
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
       assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', 33),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+      assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg', 33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    it('debería retornar " ?!,.;:-_+=$" para " ?!,.;:-_+=$" con offset 33', () => {
+      assert.equal(cipher.decode(' ?!,.;:-_+=$', 33), ' ?!,.;:-_+=$');
     });
   });
 
